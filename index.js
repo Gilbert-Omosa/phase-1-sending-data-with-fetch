88a9c81c65ef58afa001d966ccf72830cc8bc568
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-10-08 23:46:49 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name,email) {
+  const formData = {
+      name: "Steve",
+      email: "steve@steve.com",
+    };
+    
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        // Append the new id to the DOM
+        const newId = object.id;
+        document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+      })
+      .catch(function (error) {
+        // Append the error message to the DOM
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
